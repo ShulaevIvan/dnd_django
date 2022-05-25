a52c6c4358210baaf54c1e1c1a763ced81c35bfa
Shulaev <mail@ntdservice.ru>
2022-05-25 20:14:19 +0300

    fix adding user with email by control-panel

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserCreationForm
 from .models import User
 
 @admin.register(User)
 class AdminUser(UserAdmin):
-    pass
+
+    add_form = UserCreationForm
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("username", 'email', "password1", "password2"),
+            },
+        ),
+    )
