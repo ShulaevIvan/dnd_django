d636738fc018ff5198f2605f3be339b65fcfe521
Shulaev <mail@ntdservice.ru>
2022-05-25 17:18:48 +0300

    initial, create basic_auth

---
+++ users/urls.py
@@ -0,0 +1,8 @@
+from xml.etree.ElementInclude import include
+from django.urls import path, include
+from .views import Register
+
+urlpatterns = [
+    path('', include('django.contrib.auth.urls')),
+    path('register/', Register.as_view(), name='register')
+]
